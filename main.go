df23f1f9f86acdfa2375fd5d36fc6320209c49e4
13972762373 <574635222@qq.com>
2024-08-15 23:15:07 +0800

    updates workflows

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
+
+const appName = "wocr"
 
 func main() {
 	// Create an instance of the app structure
@@ -18,7 +25,7 @@ func main() {
 	model.Init(app.ctx)
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:     "OCR助手",
+		Title:     appName,
 		Width:     1024,
 		Height:    768,
 		MinHeight: 768,
@@ -28,6 +35,21 @@ func main() {
 		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
+		},
+		Mac: &mac.Options{
+			TitleBar: mac.TitleBarHiddenInset(),
+			About: &mac.AboutInfo{
+				Title:   appName,
+				Message: "A ocr tool cross-platform desktop app.\n\nCopyright © 2024",
+				Icon:    icon,
+			},
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+		},
+		Windows: &windows.Options{
+			WebviewIsTransparent:              false,
+			WindowIsTranslucent:               false,
+			DisableFramelessWindowDecorations: false,
 		},
 	})
 
